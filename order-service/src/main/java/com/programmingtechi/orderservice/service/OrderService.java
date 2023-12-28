b663b21fc9c19b3bdfec2071d16cd919c92525a3
papon <priyabrotasenpapon94@gmail.com>
2023-12-29 00:18:13 +0600

    Event driven architecute using kafka

--- order-service/src/main/java/com/programmingtechi/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programmingtechi/orderservice/service/OrderService.java
@@ -5,12 +5,14 @@ package com.programmingtechi.orderservice.service;
 import com.programmingtechi.orderservice.dto.InventoryResponse;
 import com.programmingtechi.orderservice.dto.OrderLineItemsDto;
 import com.programmingtechi.orderservice.dto.OrderRequest;
+import com.programmingtechi.orderservice.event.OrderPlacedEvent;
 import com.programmingtechi.orderservice.model.Order;
 import com.programmingtechi.orderservice.model.OrderLineItems;
 import com.programmingtechi.orderservice.repository.OrderRepository;
 import io.micrometer.tracing.Span;
 import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -26,7 +28,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
-
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
     private final Tracer tracer;
     public String placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -58,6 +60,7 @@ public class OrderService {
 
             if(isInStock){
                 orderRepository.save(order);
+                kafkaTemplate.send("notificationTopic", new OrderPlacedEvent(order.getOrderNumber()));
                 return "Order placed successfully";
             } else{
                 System.out.println("Product is not in stock. Please try again later");
